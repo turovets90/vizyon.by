acb9d0256db2288187845f016c29d4b454986c5f
turovets90 <a.turovets90@gmail.com>
2018-11-23 18:19:55 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -29,6 +29,66 @@ $(document).ready(function(){
 
 
 
+    $(window).resize(function(){
+        var header_height = $('header').outerHeight();
+        $('.section_content').css({'margin-top': header_height+'px'});
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > header_height) {
+                $('header').addClass('fixed');
+            } else {
+                $('header').removeClass('fixed');
+            }
+        });
+
+    });
+    $(window).resize();
+
+
+
+    if($('.product_slider > div').length >1){
+        $('.product_slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            //fade: true,
+            asNavFor: '.product_slider_nav'
+        });
+    }
+
+
+    $('.product_slider_nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.product_slider',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true,
+        responsive: [
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
+    });
+
+
+
+
     /*
     $('.js-example-basic-single').select2();
 
